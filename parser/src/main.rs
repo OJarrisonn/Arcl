0e92488b9525de7d5fdb2a71fc1137fc27096a28
OJarrisonn <j.h.m.t.v.10@gmail.com>
2023-12-31 11:06:36 -0300

    Base for ARCL Parser

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "../utils/grammar.pest"]
+struct ArclParser;
+
 fn main() {
-    println!("Hello, world!");
+    let source = fs::read_to_string("./test/hello_world.arcl").unwrap();
+
+
+    let program = ArclParser::parse(Rule::program, &source)
+        .expect("The syntax is wrong")
+        .next()
+        .unwrap();
+
+    let _ = dbg!(program);
 }
